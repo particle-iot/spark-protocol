d84f2e2795d5ea1993a4bb57b482b4d609aaef0a
David Middlecamp <david@spark.io>
2014-06-11 11:01:43 -0500

    cryptostream initial commit

---
+++ js/lib/CryptoStream.js
@@ -0,0 +1,83 @@
+var Transform = require('stream').Transform;
+var CryptoLib = require("./ICrypto");
+var crypto = require('crypto');
+var logger = require('../lib/logger.js');
+
+var CryptoStream = function (options) {
+    Transform.call(this, options);
+    this.key = options.key;
+    this.iv = options.iv;
+    this.encrypt = !!options.encrypt;
+};
+CryptoStream.prototype = Object.create(Transform.prototype, { constructor: { value: CryptoStream }});
+CryptoStream.prototype._transform = function (chunk, encoding, callback) {
+    try {
+
+        //assuming it comes in full size pieces
+        var cipher = this.getCipher(callback);
+        cipher.write(chunk);
+        cipher.end();
+        cipher = null;
+
+        if (!this.encrypt) {
+            //ASSERT: we just DECRYPTED an incoming message
+            //THEN:
+            //  update the initialization vector to the first 16 bytes of the encrypted message we just got
+            this.iv = new Buffer(16);
+            chunk.copy(this.iv, 0, 0, 16);
+
+            //logger.log('server: GOT ENCRYPTED CHUNK', chunk.toString('hex'));
+        }
+//        else {
+//            console.log('pre-encrypt sending: ' + chunk.toString('hex'));
+//        }
+    }
+    catch (ex) {
+        logger.error("CryptoStream transform error " + ex);
+    }
+};
+CryptoStream.prototype.getCipher = function (callback) {
+    var cipher = null;
+    if (this.encrypt) {
+        cipher = crypto.createCipheriv('aes-128-cbc', this.key, this.iv);
+    }
+    else {
+        cipher = crypto.createDecipheriv('aes-128-cbc', this.key, this.iv);
+    }
+
+    var ciphertext = null,
+        that = this;
+
+    cipher.on('readable', function () {
+        var chunk = cipher.read();
+
+        if (!ciphertext) {
+            ciphertext = chunk;
+        }
+        else {
+            ciphertext = Buffer.concat([ciphertext, chunk], ciphertext.length + chunk.length);
+        }
+    });
+    cipher.on('end', function () {
+        //var action = (that.encrypt) ? "encrypting" : "decrypting";
+        //logger.log(action + ' chunk to ', ciphertext.toString('hex'));
+
+        that.push(ciphertext);
+
+        if (that.encrypt) {
+            //logger.log("ENCRYPTING WITH ", that.iv.toString('hex'));
+            //get new iv for next time.
+            that.iv = new Buffer(16);
+            ciphertext.copy(that.iv, 0, 0, 16);
+
+            //logger.log("ENCRYPTING WITH ", that.iv.toString('hex'));
+        }
+        ciphertext = null;
+
+        callback();
+    });
+
+    return cipher;
+};
+module.exports = CryptoStream;
+
