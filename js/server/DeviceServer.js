36e540b2842026aa78f5c265adf10fe8a46e2cc8
David Middlecamp <david@spark.io>
2014-07-07 16:50:50 -0500

    adding helper function for adding core keys

--- js/server/DeviceServer.js
+++ js/server/DeviceServer.js
@@ -1,3 +1,23 @@
+/*
+*   Copyright (C) 2013-2014 Spark Labs, Inc. All rights reserved. -  https://www.spark.io/
+*
+*   This file is part of the Spark-protocol module
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License version 3
+*   as published by the Free Software Foundation.
+*
+*   Spark-protocol is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Spark-protocol.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   You can download the source here: https://github.com/spark/spark-protocol
+*/
+
 var settings = require('../settings.js');
 var CryptoLib = require('../lib/ICrypto.js');
 var SparkCore = require('../clients/SparkCore.js');
@@ -32,6 +52,18 @@ DeviceServer.prototype = {
 
     init: function () {
         this.loadCoreData();
+    },
+
+    addCoreKey: function(coreid, public_key) {
+        try{
+            var fullPath = path.join(this.options.coreKeysDir, coreid + ".pub.pem");
+            fs.writeFileSync(fullPath, public_key);
+            return true;
+        }
+        catch (ex) {
+            logger.error("Error saving new core key ", ex);
+        }
+        return false;
     },
 
 
